47b8b6326847a0d06f1c0a06be2dca528fc5125e
Ntwali-1 <ntwaliyanis@gmail.com>
2025-10-23 16:13:31 +0200

    refactor: StudentMain

--- src/StudentMain.java
+++ src/StudentMain.java
@@ -3,6 +3,15 @@ import java.io.*;
 public class StudentMain {
     public static void main(String[] args) {
         Student student = new Student();
+        File file = new File("C:\\Users\\HP\\OneDrive\\Documents\\student.ser");
+
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file))) {
+            out.writeObject(student);
+        }catch (IOException e) {
+            //
+        }
+
+
         student.setFirstName("John");
         student.setLastName("Doe");
     }
