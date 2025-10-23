51d2257f460f2b7c3051368f442ffe89091941ea
Ntwali-1 <ntwaliyanis@gmail.com>
2025-10-23 16:09:28 +0200

    feat: Serialization

---
+++ src/StudentMain.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class StudentMain {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFirstName("John");
+        student.setLastName("Doe");
+    }
+}
